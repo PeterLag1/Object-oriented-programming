4ff9c50a69d2994cb4c7cfe8537164c44c2b7e96
PeterLag <alfonsozap120@gmail.com>
2022-04-29 18:35:36 -0600

    29/04/22 - 6:35 PM

---
+++ Guia1/Tarea2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int n0, n1, n2, n3, res;
+
+            do
+            {
+                Console.WriteLine("Calculadora\n1 -> Suma\n2 -> Resta\n3 -> Multiplicacion\n4 -> Divicion");
+            n0 = int.Parse(Console.ReadLine());
+                switch (n0)
+                {
+                    case 1:
+                        Console.WriteLine("\tSUMA\nIngrese los 3 datos");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = int.Parse(Console.ReadLine());
+                        n3 = int.Parse(Console.ReadLine());
+                        res = n1 + n2 + n3;
+                        Console.WriteLine($"El resultado es: {res}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\tRESTA\nIngrese los 3 datos");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = int.Parse(Console.ReadLine());
+                        n3 = int.Parse(Console.ReadLine());
+                        res = n1 - n2 - n3;
+                        Console.WriteLine($"El resultado es: {res}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\tMULTIPLICACION\nIngrese los 3 datos");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = int.Parse(Console.ReadLine());
+                        n3 = int.Parse(Console.ReadLine());
+                        res = n1 * n2 * n3;
+                        Console.WriteLine($"El resultado es: {res}");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\tDIVICION\nIngrese los 3 datos");
+                        n1 = int.Parse(Console.ReadLine());
+                        n2 = Console.Read();
+                        n3 = Console.Read();
+                        res = n1 / n2 / n3;
+                        Console.WriteLine($"El resultado es: {res}");
+                        break;
+
+                    default:
+                        Console.WriteLine("ERROR: Valor no valido");
+                        break;
+                }
+
+                Console.WriteLine("Desea salir?\n1 -> No\n5 -> Si");
+                n0 = int.Parse(Console.ReadLine());
+
+            } while (n0 != 5);
+        }
+    }
+}
---
+++ Guia2/Ejercicio1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            do
+            {
+                Console.WriteLine("\tSELECCIONE EL METODO\n1 -> Array\n2 -> List\n3 -> Salir");
+                menu = int.Parse(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        int num;
+                        Console.WriteLine("Cunatos estudiantes desea ingrear: ");
+                        num = int.Parse(Console.ReadLine());
+                        int[] Studenst = new int[num];
+                        for (int i = 0; i < num; i++)
+                        {
+                            Console.WriteLine($"Ingrese la nota del estudiante {i + 1}: ");
+                            Studenst[i] = int.Parse(Console.ReadLine());
+                            if (Studenst[i] < 60)
+                            {
+                                Console.WriteLine("Reprobado");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Aprobado");
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        List<int> StuLis = new List<int>();
+            
+                        Console.WriteLine("Ingrese el numero de estudiantes");
+                        int n = int.Parse(Console.ReadLine());
+                     
+                        for (int i = 0; i < n; i++)
+                        {
+                           Console.WriteLine($"Ingrese la onta del estudiante {i + 1}");
+                           StuLis.Add(int.Parse(Console.ReadLine()));
+                            if (StuLis[i] < 60)
+                            {
+                                Console.WriteLine("Reprobado");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Aprobado");
+                            }
+                        }
+                        
+
+                        break;
+
+                    default:
+                        Console.WriteLine("ERROR: Valor no valido");
+                        break;
+                }
+            } while (menu != 3);
+        }
+    }
+}
